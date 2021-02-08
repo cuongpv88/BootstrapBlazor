72dfff42c1445d8236c7f04053c0400870c7f7ad
Argo-Unicom <argo@163.com>
2021-02-08 14:28:50 +0800

    fix: 修复 resx 模式 DisplayName 与 Validation 失效问题

--- src/BootstrapBlazor/Extensions/FieldIdentifierExtensions.cs
+++ src/BootstrapBlazor/Extensions/FieldIdentifierExtensions.cs
@@ -4,6 +4,8 @@
 
 using BootstrapBlazor.Components;
 using BootstrapBlazor.Localization.Json;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Concurrent;
 using System.ComponentModel;
@@ -79,13 +81,16 @@ namespace Microsoft.AspNetCore.Components.Forms
                     }
                     if (!string.IsNullOrEmpty(dn))
                     {
-                        var localizer = JsonStringLocalizerFactory.CreateLocalizer(cacheKey.Type);
-                        var stringLocalizer = localizer[dn];
-                        if (!stringLocalizer.ResourceNotFound)
+                        var resxType = ServiceProviderHelper.ServiceProvider.GetRequiredService<IOptions<JsonLocalizationOptions>>();
+                        if (resxType.Value.ResourceManagerStringLocalizerType != null)
                         {
-                            dn = stringLocalizer.Value;
+                            var localizer = JsonStringLocalizerFactory.CreateLocalizer(resxType.Value.ResourceManagerStringLocalizerType);
+                            var stringLocalizer = localizer[dn];
+                            if (!stringLocalizer.ResourceNotFound)
+                            {
+                                dn = stringLocalizer.Value;
+                            }
                         }
-
                         // add display name into cache
                         DisplayNameCache.GetOrAdd(cacheKey, key => dn);
                     }
